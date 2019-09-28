0b74fedd12064ded3dcfac6fe34af7c601962309
GerardoSant <gbuenclima@gmail.com>
2019-09-28 21:50:01 +0200

    Initial commit: Working version of project. Has view from user inputs from keyboard. Test implemented over HowMuchMoneyCanIMake class, but not over view.

---
+++ src/main/java/HowMuchMoneyCanIMakeView.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+import static java.lang.Double.parseDouble;
+
+public class HowMuchMoneyCanIMakeView {
+    private static Scanner inputScanner = new Scanner (System.in);
+
+    /*public static void main(String[] args) {
+        start();
+    }*/
+    public static void start(){
+        System.out.print("Tu capital será: "+ HowMuchMoneyCanIMake.calculate(getUserInitialCapital(),getUserAnnualAddition(),
+                getUserInterestRate(), getUserYearsToGrow()));
+    }
+
+
+    private static int getUserYearsToGrow() {
+        return (int) askForUserInput("Años para crecer: ");
+    }
+
+
+    private static double getUserInterestRate() {
+        return askForUserInput("Rentabilidad anual: ");
+    }
+
+    private static int getUserAnnualAddition() {
+        return (int) askForUserInput("Adición anual sobre el capital: ");
+    }
+
+    private static double getUserInitialCapital() {
+        return (int) askForUserInput("Capital inicial: ");
+    }
+
+    private static double askForUserInput(String sysout) {
+        System.out.print(sysout);
+        return getUserInput(inputScanner);
+    }
+
+    private static double getUserInput(Scanner inputScanner) {
+        try{
+            return inputScanner.nextDouble();
+        } catch(Exception e){
+            System.out.println("Has insertado un valor que no es un número. Debes insertar un valor númerico.");
+            inputScanner.next();
+            return getUserInput(inputScanner);
+        }
+    }
+
+
+}
---
+++ src/test/java/HowMuchMoneyCanIMake_.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(org.junit.runners.Parameterized.class)
+public class HowMuchMoneyCanIMake_ {
+    private int initialCapital;
+    private int annualAddition;
+    private double interestRate;
+    private int yearsToGrow;
+    private int finalcapital;
+
+    public HowMuchMoneyCanIMake_(int initialCapital, int annualAddition, double interestRate, int yearsToGrow, int finalcapital) {
+        this.initialCapital = initialCapital;
+        this.annualAddition = annualAddition;
+        this.interestRate = interestRate;
+        this.yearsToGrow = yearsToGrow;
+        this.finalcapital = finalcapital;
+    }
+
+    @Test
+    public void execute(){
+        assertThat(HowMuchMoneyCanIMake.calculate(initialCapital,annualAddition,interestRate,yearsToGrow))
+                .isEqualTo(finalcapital);
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] cases(){
+        return new Object[][]{
+                {0,0,0,0,0},
+                {-100,0,10,5,-1},
+                {100,0,0,0,100},
+                {100,0,0,1,100},
+                {100,0,10,1,110},
+                {100,100,10,1,210},
+                {100,100,0,0,100},
+                {100,100,10,0,100},
+                {100,0,10,0,100},
+                {100,100,10,2,331},
+                {100,100,10,5,771},
+                {100,1000,10,5,6266},
+                {100,1000,15,10,20708},
+                {1000,1000,15,10,24349},
+
+
+        };
+    }
+
+
+}
+
